622036d3e8adb854517345e912cae1c5b5a13ab7
Taylor Bailey <taylor.williams.bailey@gmail.com>
2018-02-14 15:39:32 -0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+"use strict";
+
+console.log("main js is in the house");
+
+let MathModule = require("./math");
+console.log("MathModule", MathModule);
+console.log("add", MathModule.add(3,5));
+
+
+
+
+
+
+
+
